671fe00ace650f66e8a0d7d521ad18145101c596
nelson-almeida-22304590 <a22304590@alunos.ulht.pt>
2025-05-15 16:25:18 +0000

    company models added, very basic index.html and base.html added

--- crmsolar/models.py
+++ crmsolar/models.py
@@ -1,8 +1,23 @@
 from django.db import models
+from django.db.models import IntegerField
+
 
 # Create your models here.
 
 class Company(models.Model):
-    name = models.CharField(max_length=100)
+    nif = IntegerField(primary_key=True)
+    name = models.CharField(max_length=100, null=True, blank=True)
+    address = models.CharField(max_length=240, null=True, blank=True)
+    city  = models.CharField(max_length=50, null=True, blank=True)
+    zip_code = models.CharField(max_length=10, null=True, blank=True)
+    county = models.CharField(max_length=50, null=True, blank=True)
+    telephone = models.IntegerField(null=True, blank=True)
+    email = models.CharField(max_length=100, null=True, blank=True)
+    site = models.CharField(max_length=100, null=True, blank=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    last_modification_date = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
+
 
 
--- crmsolar/views.py
+++ crmsolar/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Company
 
+def index_view(request):
+    companies = Company.objects.all()
+    return render(request, 'crmsolar/index.html', {'companies': companies})
 # Create your views here.
