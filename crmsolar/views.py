0f497d4aab7c1f037ca777ed29310e011e19cadf
nelsonalmeida2 <nelson10sonia@gmail.com>
2025-07-05 17:19:54 +0100

    Add login form and update base template with logout link

--- crmsolar/views.py
+++ crmsolar/views.py
@@ -1,7 +1,48 @@
-from django.shortcuts import render
-from .models import Company
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
+from .models import (
+    District,
+    Country,
+    Address,
+    Segment,
+    BuildingType,
+    Observation,
+    Contact,
+    Company,
+    CompanyGroup,
+    Provider,
+    CPE,
+    OpportunityStatus,
+    Product,
+    Opportunity,
+    CalendarEntry,
+    TaskType,
+    Task,
+)
+
+
+@login_required
 def index_view(request):
     companies = Company.objects.all()
     return render(request, 'crmsolar/index.html', {'companies': companies})
-# Create your views here.
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')  # Redireciona para a página inicial depois de login
+        else:
+            messages.error(request, 'Invalid username or password.')
+    return render(request, 'crmsolar/login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
